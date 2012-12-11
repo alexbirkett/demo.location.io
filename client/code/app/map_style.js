70c491e2f630053aacec37ef701e3873f8778521
Alex Birkett <alex@birkett.co.uk>
2012-12-11 16:39:53 +0100

    changed map style

--- client/code/app/map_style.js
+++ client/code/app/map_style.js
@@ -4,23 +4,8 @@ exports.style = [ {
 		hue : '#00dd00'
 	} ]
 }, {
-	featureType : 'road',
-	stylers : [ {
-		hue : '#dd0000'
-	} ]
-}, {
 	featureType : 'water',
 	stylers : [ {
-		hue : '#000040'
-	} ]
-}, {
-	featureType : 'poi.park',
-	stylers : [ {
-		visibility : 'off'
-	} ]
-}, {
-	featureType : 'road.arterial',
-	stylers : [ {
-		hue : '#ffff00'
+		hue : '#453D91'
 	} ]
 }];
